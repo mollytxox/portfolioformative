95193730b9b65f7ce209655cd3eb49d9d3d0c0c3
jaruuude <jaredrclarke@outlook.co.nz>
2022-08-25 10:53:07 +1200

    added nothing

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,6 @@
 
-
+const result = document.getElementById("result");
+console.log("hi!");
 
 let showAllProjects = () => {
     $.ajax({
@@ -9,11 +10,31 @@ let showAllProjects = () => {
         success: (projects) => {
             console.log(projects);
             console.log("is this running??");
+            renderProjects(projects);
         },
         error: (error) => {
             console.log(error);
         }
     })
 }
+
+
+
+let renderProjects = (projects) => {
+    console.log("the render projects function is working");
+    result.innerHTML = "";
+    projects.forEach((item) => {
+        result.innerHTML += `
+        <div class="product-wrapper" id="${item._id}">
+            <h3>${item.name}</h3>
+            <img src="${item.img_url}">
+            <h5>${item.author}</h5>
+            
+        </div>
+        `;
+    });
+
+};
+
 
 showAllProjects();
